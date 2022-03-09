3916ac6742f38cdb592ff51560b459597ffe2297
Joowd <joowdx@pm.me>
2022-03-09 15:24:02 +0800

    Chunk bulk insertions

--- routes/web.php
+++ routes/web.php
@@ -22,33 +22,41 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return dd(Employee::with([
-        'logs' => fn ($e) => $e->whereMonth('time', 2)->whereYear('time', 2022)
-    ])->first()->logs->map(fn ($e) => $e->time));
 
-    // File::lines('../PGSO.csv')->skip(1)->filter(fn($e) => $e)->map(fn($e) => Str::of($e)->explode(','))->each(function ($e) {
-    //     Employee::create([
-    //         'biometrics_id' => $e[0],
-    //         'name' => [
-    //             'last' => $e[1],
-    //             'first' => $e[2],
-    //             'middle' => $e[3],
-    //             'extension' => $e[4],
-    //         ],
-    //         'regular' => (bool) $e[5],
-    //         'user_id' => Auth::user()->id ?? 1,
-    //     ]);
-    // });
+    // return dd(Employee::with([
+    //     'logs' => fn ($e) => $e->whereMonth('time', 2)->whereYear('time', 2022)
+    // ])->first()?->logs->map(fn ($e) => $e->time));
 
-    // return File::lines('../attlog.dat')
-    //     ->filter(fn ($e) => $e)
-    //     ->map(fn ($e) => explode("\t", $e))
-    //     // ->first();
-    //     ->each(fn ($e) => Log::create([
-    //         'biometrics_id' => trim($e[0]),
-    //         'time' => Carbon::createFromTimeString($e[1]),
-    //         'state' => join(' ', array_slice($e, 2)),
-    //     ]));
+    File::lines('../PGSO.csv')
+        ->skip(1)
+        ->filter(fn($e) => $e)
+        ->map(fn($e) => str($e)->explode(','))
+        ->map(fn ($e) => [
+            'biometrics_id' => $e[0],
+            'name' => json_encode([
+                'last' => $e[1],
+                'first' => $e[2],
+                'middle' => $e[3],
+                'extension' => $e[4],
+            ]),
+            'regular' => (bool) $e[5],
+            'user_id' => Auth::user()->id ?? 1,
+        ])
+        ->chunk(100)
+        ->map(fn ($e) => $e->toArray())
+        ->each(fn ($e) => Employee::insert($e));
+
+    return File::lines('../1_attlog.dat')
+        ->filter(fn ($e) => $e)
+        ->map(fn ($e) => explode("\t", $e))
+        ->map(fn ($e) => [
+            'biometrics_id' => trim($e[0]),
+            'time' => Carbon::createFromTimeString($e[1]),
+            'state' => join('', collect(array_slice($e, 2))->map(fn ($e) => $e > 1 ? 1 : $e)->toArray()),
+        ])
+        ->chunk(1000)
+        ->map(fn ($e) => $e->toArray())
+        ->each(fn ($e) => Log::insert($e));
 
     // return Inertia::render('Welcome', [
     //     'canLogin' => Route::has('login'),
