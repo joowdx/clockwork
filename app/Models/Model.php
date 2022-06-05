18bd37f8174b271adad2a6d90c5309011a2ff681
Joowd <joowdx@gmail.com>
2022-06-05 21:02:16 +0800

    Remove compoships dependency

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -3,11 +3,10 @@
 namespace App\Models;
 
 use App\Traits\HasUniversallyUniqueIdentifier;
-use Awobaz\Compoships\Compoships;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model as Main;
 
 class Model extends Main
 {
-    use Compoships, HasFactory, HasUniversallyUniqueIdentifier;
+    use HasFactory, HasUniversallyUniqueIdentifier;
 }
