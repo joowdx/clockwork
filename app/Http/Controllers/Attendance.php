8bbbf3bde400adabdd6921ff4d01a9d1678a0989
Joowd <joowdx@gmail.com>
2022-08-07 17:42:45 +0800

    Add attendance view for readonly users

--- app/Http/Controllers/Attendance.php
+++ app/Http/Controllers/Attendance.php
@@ -11,45 +11,177 @@ class Attendance extends Component
 {
     use WithPagination;
 
-    protected $queryString = ['search', 'from'];
+    protected $queryString = [
+        'search',
+        'from',
+        'start',
+        'end',
+        'active',
+        'office'
+    ];
 
-    protected $employee, $scanner;
+    public $url;
 
     public $search;
 
-    public $from = 'office';
+    public $from = 'offices';
 
-    public $active = true;
+    public $active = '';
+
+    public $start;
+
+    public $end;
 
     public $selected;
 
     public $office;
+
+    public function mount()
+    {
+        $this->selected['scanners'] = $this->selected['scanners'] = app(ScannerRepository::class)->all()
+            ->when($this->from === 'offices', fn($scanners) => $scanners->filter(fn ($scanner) => str_contains(strtolower($scanner->name), 'coliseum')))
+            ->mapWithKeys(fn ($scanner) => [$scanner->id => true])
+            ->toArray();
+
+        $this->start = today()->startOfWeek()->format('Y-m-d');
+
+        $this->end = today()->endOfMonth()->format('Y-m-d');
+    }
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
 
     public function updatedFrom()
     {
         $this->office = $this->from !== 'offices'
             ? $this->office
             : '';
+
+        $this->search = '';
+
+        $this->selected['employees'] = [];
+
+        $this->selected['offices'] = [];
+
+        $this->selected['scanners'] = app(ScannerRepository::class)->all()
+            ->when($this->from === 'offices', fn($scanners) => $scanners->filter(fn ($scanner) => str_contains(strtolower($scanner->name), 'coliseum')))
+            ->mapWithKeys(fn ($scanner) => [$scanner->id => true])
+            ->toArray();
+
+        $this->resetValidation();
+
+        $this->resetPage();
     }
 
-    public function boot()
+    public function updatedOffice()
     {
-        $this->employee = app(EmployeeRepository::class);
+        $this->resetPage();
+    }
 
-        $this->scanner = app(ScannerRepository::class);
+    public function updatedActive()
+    {
+        $this->resetPage();
     }
 
     public function render()
     {
+        $scanner = app(ScannerRepository::class);
+
+        $employee = app(EmployeeRepository::class);
+
         return view('attendance', [
-            'scanners' => $this->scanner->query()->get(),
+            'scanners' => $scanner->query()
+                ->when($this->search, fn ($query) => $query->where('name', 'like', "%{$this->search}%"))
+                ->get(),
             'employees' => $this->from === 'employees'
-                ? $this->employee->query()
-                    ->when(is_bool($this->active), fn ($query) => $query->whereActive($this->active))
+                ? $employee->query()
+                    ->when($this->active !== '', fn ($query) => $query->whereActive($this->active))
                     ->when($this->office, fn ($query) => $query->whereOffice(strtoupper($this->office)))
-                    ->get()
+                    ->paginate(50)
                 : [],
-            'offices' => $this->employee->model()->select('office')->distinct('office')->get()->map->office->prepend('')->unique(),
+            'offices' => ($offices = $employee->model()->select('office')->distinct('office')->get()->map->office)->empty() ? $offices->prepend('') : $offices->prepend('')->unique(),
         ]);
+    }
+
+    protected function messages()
+    {
+        return [
+            'selected.employees.required_if' => 'Please select an employee.',
+            'selected.offices.required_if' => 'Please select an office.',
+            'selected.scanners.required' => 'Please select a scanner.',
+        ];
+    }
+
+    protected function rules()
+    {
+        return [
+            'selected.employees' => 'required_if:from,employees|array',
+            'selected.employees.*' => 'uuid|exists:employees,id',
+            'selected.offices' => 'required_if:from,offices|array',
+            'selected.offices.*' => 'string|exists:employees,office',
+            'selected.scanners' => 'required|array',
+            'selected.scanners.*' => 'uuid|exists:scanners,id',
+            'start' => 'required|date:Y-m-d',
+            'end' => 'required_if:from,employees|date:Y-m-d',
+        ];
+    }
+
+    protected function prepareForValidation($attributes)
+    {
+        $attributes['selected'] = collect($attributes['selected'])
+            ->map(
+                fn ($att) => collect($att)->filter()->keys()->toArray()
+            )
+            ->toArray();
+
+        return $attributes;
+    }
+
+    public function generate()
+    {
+        if ($this->url) {
+            $this->url = null;
+
+            return;
+        }
+
+        $this->validate();
+
+        $this->url = match ($this->from) {
+            'employees' => route('print', [
+                'view' => 'employee',
+                'period' => 'custom',
+                'from' => $this->start,
+                'to' => $this->end,
+                'employees' => collect(@$this->selected['employees'])
+                    ->filter()
+                    ->keys()
+                    ->values()
+                    ->toArray(),
+                'scanners' => collect(@$this->selected['scanners'])
+                    ->filter()
+                    ->keys()
+                    ->values()
+                    ->toArray(),
+            ]),
+            'offices' => route('print', [
+                'view' => 'office',
+                'date' => $this->start,
+                'offices' => collect(@$this->selected['offices'])
+                    ->filter()
+                    ->keys()
+                    ->map(fn ($office) => strtoupper($office))
+                    ->values()
+                    ->toArray(),
+                'scanners' => collect(@$this->selected['scanners'])
+                    ->filter()
+                    ->keys()
+                    ->values()
+                    ->toArray(),
+            ]),
+        };
+
     }
 }
--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -3,124 +3,180 @@
 </x-slot>
 
 <div class="py-12">
-    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8" x-data="{
+        url: @entangle('url'),
+        generated: false,
+    }">
         <div class="overflow-hidden text-white bg-gray-900 shadow-xl sm:rounded-lg">
 
-            <div class="flex px-5 pt-5 space-x-3" x-data="{ from: @entangle('from') }">
-                <div class="w-1/5">
+            <div class="grid grid-cols-12 gap-3 p-5" x-data="{ from: @entangle('from') }">
+                <div class="col-span-6 md:col-span-3">
                     <x-jet-label for="search" class="text-white uppercase" value="{{ __('Search') }}" />
-                    <x-jet-input id="search" class="inline-flex items-center w-full px-4 py-2 mt-1 text-xs font-semibold tracking-widest text-white uppercase transition bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25" type="text" name="search" :value="old('search')" required autofocus x-bind:placeholder="from" wire:model="search" />
+                    <x-jet-input id="search" class="inline-flex items-center w-full px-4 py-2 mt-1 text-xs font-semibold tracking-widest text-white uppercase transition bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25" type="text" name="search" :value="old('search')" required autofocus x-bind:placeholder="'Scanners / ' + from" wire:model.debounce.250ms="search" disabled="{{ (bool) $url }}" />
                 </div>
-                <div class="w-1/5">
+                <div class="col-span-6 md:col-span-3">
                     <x-jet-label for="from" class="text-white uppercase" value="{{ __('From') }}" />
-                    <select id="from" class="inline-flex items-center w-full px-4 py-2 mt-1 text-xs font-semibold tracking-widest text-white uppercase transition bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25" wire:model="from">
+                    <select id="from" class="inline-flex items-center w-full px-4 py-2 mt-1 text-xs font-semibold tracking-widest text-white uppercase transition bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25" wire:model="from" @disabled($url) >
                         <option>employees</option>
                         <option>offices</option>
                     </select>
                 </div>
-                <div class="w-1/5">
-                    <x-jet-label for="office" class="text-white uppercase" value="{{ __('Office') }}" />
-                    <select id="offices" class="inline-flex items-center w-full px-4 py-2 mt-1 text-xs font-semibold tracking-widest text-white uppercase transition bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25" wire:model="office" @disabled($from === 'offices')>
-                        @foreach ($offices as $office)
-                            <option> {{ $office }} </option>
-                        @endforeach
-                    </select>
+                <div class="col-span-6 md:col-span-3">
+                    <x-jet-label for="start" class="text-white uppercase" value="{{ $from == 'offices' ? __('Date') : __('Start') }}" />
+                    <x-jet-input id="start" class="inline-flex items-center w-full px-4 py-2 mt-1 text-xs font-semibold tracking-widest text-white uppercase transition bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25" type="date" name="start" :value="old('start')" required autofocus x-bind:placeholder="from" wire:model="start" disabled="{{ (bool) $url }}" />
                 </div>
-                <div class="w-1/5">
+                @if ($from !== 'offices')
+                    <div class="col-span-6 md:col-span-3">
+                        <x-jet-label for="end" class="text-white uppercase" value="{{ __('End') }}" />
+                        <x-jet-input id="end" class="inline-flex items-center w-full px-4 py-2 mt-1 text-xs font-semibold tracking-widest text-white uppercase transition bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25" type="date" name="end" :value="old('end')" required autofocus x-bind:placeholder="from" wire:model="end" disabled="{{ (bool) $url }}" />
+                    </div>
+                    <div class="col-span-6 md:col-span-3">
+                        <x-jet-label for="office" class="text-white uppercase" value="{{ __('Office') }}" />
+                        <select id="offices" class="inline-flex items-center w-full px-4 py-2 mt-1 text-xs font-semibold tracking-widest text-white uppercase transition bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25" wire:model="office" @disabled($url) >
+                            @foreach ($offices as $office)
+                                <option> {{ $office }} </option>
+                            @endforeach
+                        </select>
+                    </div>
+                @endif
+                <div class="col-span-6 md:col-span-3">
                     <x-jet-label for="active" class="text-white uppercase" value="{{ __('Active') }}" />
-                    <select id="active" class="inline-flex items-center w-full px-4 py-2 mt-1 text-xs font-semibold tracking-widest text-white uppercase transition bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25" wire:model="active">
-                        <option value="0">All</option>
+                    <select id="active" class="inline-flex items-center w-full px-4 py-2 mt-1 text-xs font-semibold tracking-widest text-white uppercase transition bg-gray-800 border border-transparent rounded-md hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25" wire:model="active" @disabled($url) >
+                        <option value=""></option>
                         <option value="true">Active</option>
                         <option value="false">Inactive</option>
                     </select>
                 </div>
             </div>
 
-            <div class="grid items-start grid-cols-12 p-5 md:space-x-3">
-                @switch($from)
-                    @case('offices')
-                        <table class="col-span-12 table-auto md:col-span-4">
-                            <thead>
-                                <tr>
-                                    <td class="w-1 pr-3">
-                                        <x-jet-checkbox />
-                                    </td>
-                                    <td class="font-bold uppercase">
-                                        Office
-                                    </td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($offices->filter() as $office)
-                                    <tr>
-                                        <td class="w-1 pr-3">
-                                            <x-jet-checkbox />
-                                        </td>
-                                        <td>
-                                            {{ $office }}
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                        @break
-                    @case('employees')
-                        <table class="col-span-12 table-auto md:col-span-4">
-                            <thead>
-                                <tr>
-                                    <td class="w-1 pr-3">
-                                        <x-jet-checkbox />
-                                    </td>
-                                    <td class="font-bold uppercase">
-                                        Employees
-                                    </td>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($employees as $employee)
-                                    <tr>
-                                        <td class="w-1 pr-3">
-                                            <x-jet-checkbox />
-                                        </td>
-                                        <td>
-                                            {{ $employee->name_format->fullStartLastInitialMiddle }}
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                        @break
-                    @default
-                @endswitch
-
-                <table class="col-span-12 table-auto md:col-span-4">
-                    <thead>
-                        <tr>
-                            <td class="w-1 pr-3">
-                                <x-jet-checkbox />
-                            </td>
-                            <td class="font-bold uppercase">
-                                Scanner
-                            </td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($scanners as $scanner)
-                            <tr>
-                                <td class="w-1 pr-3">
-                                    <x-jet-checkbox wire:model="selected.scanners.{{ $scanner->id }}" />
-                                </td>
-                                <td>
-                                    {{ $scanner->name }}
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+            <div class="flex justify-end gap-3 px-5">
+                <x-jet-button @click="generated = !url" wire:click="generate" x-html="url ? 'Close' : 'Generate'" wire:target="generate" wire:loading.attr="disabled"> Generate </x-jet-button>
+                <x-jet-button @click="document.getElementById('print').contentWindow.print()" x-bind:disabled="! url && ! generated" disabled wire:target="generate" wire:loading.attr="disabled"> Print </x-jet-button>
             </div>
 
+            @if (! empty($errors->all()))
+                <div class="px-5 text-red-500">
+                    @foreach ($errors->all() as $error)
+                        <p>{{ $error }}</p>
+                    @endforeach
+                </div>
+            @endif
+
+            @if ($url)
+                <div class="flex justify-center p-5 ">
+                    <iframe id="print" class="w-full h-screen rounded lg:w-3/4" x-bind:src="url" x-bind:title="@entangle('title')" x-on:load="generated = true"></iframe>
+                </div>
+            @else
+                <div class="grid items-start grid-cols-12 gap-3 p-5">
+                    <div class="col-span-12 md:col-span-4">
+                        <table class="table-auto">
+                            <thead>
+                                <tr>
+                                    <td colspan="2" class="font-bold uppercase">
+                                        Scanner ({{ collect($selected['scanners'])->filter()->count() }})
+                                    </td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($scanners as $scanner)
+                                    <tr>
+                                        <td class="w-1 pr-3">
+                                            <x-jet-checkbox wire:model="selected.scanners.{{ $scanner->id }}" />
+                                        </td>
+                                        <td>
+                                            {{ $scanner->name }}
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td cols-pan="2" class="text-gray-500">
+                                            We've come up empty.
+                                        </td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                    @switch($from)
+                        @case('offices')
+                            <div class="col-span-12 md:col-span-4">
+                                <table class="table-auto">
+                                    <thead>
+                                        <tr>
+                                            <td colspan="2" class="font-bold uppercase">
+                                                Offices ({{ collect(@$selected['offices'])->filter()->count() }})
+                                            </td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @forelse ($offices->filter() as $office)
+                                            <tr>
+                                                <td class="w-1 pr-3">
+                                                    <x-jet-checkbox wire:model="selected.offices.{{ $office }}"/>
+                                                </td>
+                                                <td>
+                                                    {{ $office }}
+                                                </td>
+                                            </tr>
+                                        @empty
+                                            <tr>
+                                                <td cols-pan="2" class="text-gray-500">
+                                                    We've come up empty.
+                                                </td>
+                                            </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+                            @break
+                        @case('employees')
+                            <div class="col-span-12 md:col-span-4">
+                                <table class="table-auto">
+                                    <thead>
+                                        <tr>
+                                            <td colspan="2" class="font-bold uppercase">
+                                                Employees ({{ collect(@$selected['employees'])->filter()->count() }})
+                                            </td>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @forelse ($employees as $employee)
+                                            <tr>
+                                                <td class="w-1 pr-3">
+                                                    <x-jet-checkbox wire:model="selected.employees.{{ $employee->id }}"/>
+                                                </td>
+                                                <td>
+                                                    {{ $employee->name_format->fullStartLastInitialMiddle }}
+                                                </td>
+                                            </tr>
+                                        @empty
+                                            <tr>
+                                                <td cols-pan="2" class="text-gray-500">
+                                                    We've come up empty.
+                                                </td>
+                                            </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                                {{ $employees->links() }}
+                            </div>
+                            @break
+                        @default
+                    @endswitch
+                </div>
+            @endif
 
 
         </div>
     </div>
 </div>
+
+@push('head')
+    <style>
+        :root {
+            color-scheme: dark;
+        }
+
+        [x-cloak] { display: none !important; }
+    </style>
+@endpush
