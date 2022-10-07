73044a06d6dc6209b1055b231cdcf62220a025fd
joowdx@gmail.com <joowdx@pm.me>
2022-10-07 12:24:44 +0800

    Remove enum

--- app/Models/TimeLog.php
+++ app/Models/TimeLog.php
@@ -132,12 +132,3 @@ class TimeLog extends Model
             && $this->user_id === $timeLog->user_id;
     }
 }
-
-
-enum TimeLogStates
-{
-    case IN1;
-    case IN2;
-    case OUT1;
-    case OUT2;
-}
