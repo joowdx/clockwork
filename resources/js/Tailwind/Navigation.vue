04166ee0b6881acb3f70080939d742eecb1e88c1
Joowd <joowdx@pm.me>
2023-05-05 13:49:09 +0800

    Remove theme toggle

--- resources/js/Tailwind/Navigation.vue
+++ resources/js/Tailwind/Navigation.vue
@@ -30,8 +30,6 @@
                     </div>
                 </div>
                 <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <ColorSchemeToggle />
-                    <!-- Profile dropdown -->
                     <Menu as="div" class="relative ml-3">
                         <div>
                             <MenuButton class="flex text-sm rounded-full">
