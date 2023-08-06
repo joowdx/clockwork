5146122e58c519fcfdd37d18b0ba185912d6b9c7
joowdx <joowdx@gmail.com>
2023-08-06 14:04:42 +0800

    Use lowercase on username column

--- resources/js/Pages/Users/Index.vue
+++ resources/js/Pages/Users/Index.vue
@@ -77,7 +77,7 @@ const showUserModal = (e) => {
                         <td>
                             {{ index }}
                         </td>
-                        <td>
+                        <td class="lowercase">
                             {{ row.username }}
                         </td>
                         <td>
