e72567dd63876b94e1ff85429a215063391395ee
Joowd <joowdx@gmail.com>
2023-09-19 21:43:10 +0800

    Add uploads migration

---
+++ database/migrations/2023_09_19_190655_create_uploads_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\Scanner;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('uploads', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('ip_address');
+            $table->string('user_name');
+            $table->string('scanner_name');
+            $table->dateTime('time');
+            $table->jsonb('data')->default("{}");
+            $table->string('type')->nullable();
+            $table->foreignIdFor(Scanner::class)
+                ->nullable()
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+            $table->foreignIdFor(User::class)
+                ->constrained()
+                ->cascadeOnUpdate()
+                ->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('uploads');
+    }
+};
