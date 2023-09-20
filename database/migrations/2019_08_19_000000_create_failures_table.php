f8a8212af25a9424ec3379fe73335654816ec4f7
Joowd <joowdx@pm.me>
2023-09-20 08:37:43 +0800

    Change failed jobs table to failures

--- database/migrations/2019_08_19_000000_create_failures_table.php
+++ database/migrations/2019_08_19_000000_create_failures_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,9 +14,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->string('uuid')->unique();
+        Schema::create('failures', function (Blueprint $table) {
+            $table->uuid('id')->primary()->default(DB::raw('gen_random_uuid()'));
+            $table->uuid('uuid')->unique();
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
@@ -31,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('failures');
     }
 };
