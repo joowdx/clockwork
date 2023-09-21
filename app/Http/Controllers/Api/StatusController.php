810bd141e4d896eafd4164c679358c6b0b80d3cc
Joowd <joowdx@pm.me>
2023-09-21 09:26:40 +0800

    Add additional data in status

--- app/Http/Controllers/Api/StatusController.php
+++ app/Http/Controllers/Api/StatusController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Enums\UserRole;
 use App\Http\Controllers\Controller;
+use App\Models\Scanner;
 use Illuminate\Http\Request;
 
 class StatusController extends Controller
@@ -12,6 +14,14 @@ class StatusController extends Controller
      */
     public function __invoke(Request $request)
     {
+        $user=  $request->user();
+
+        $scanners = match($user->type) {
+            UserRole::ADMINISTRATOR, UserRole::DEVELOPER, UserRole::USER => $user->scanners,
+            UserRole::DEPARTMENT_HEAD => Scanner::with(['lastUpload', 'latestTimelog'])->whereHas('employees', fn ($q) => $q->whereIn('office', $user->offices))->get(),
+            default => [],
+        };
+
         return [
             'app' => [
                 'name' => config('app.name'),
@@ -19,10 +29,16 @@ class StatusController extends Controller
                 'version' => app()->version(),
             ],
             'auth' => [
-                'user' => $request->user()->load(['employee'])->makeHidden(['type', 'disabled'])->toArray(),
+                'user' => $user->load(['employee'])->makeHidden(['type', 'disabled'])->toArray(),
                 'guard' => collect(array_keys(config('auth.guards')))->first(fn ($g) => auth()->guard($g)->check()),
             ],
             'uptime' => now()->diffForHumans(config('app.start_time'), true),
+            'timelogs' => [
+                'last_update' => ($uploads = $scanners->pluck('lastUpload')->filter()->map->time->toArray()) ? max($uploads) : null,
+                'latest_data' => ($timelogs = $scanners->pluck('latestTimelog')->filter()->map->time->toArray()) ? max($timelogs) : null,
+                'scanners_last_update' => $scanners->mapWithKeys(fn ($scanner) => [$scanner->name => $scanner->lastUpload?->time]),
+                'scanners_latest_data' => $scanners->mapWithKeys(fn ($scanner) => [$scanner->name => $scanner->latestTimelog?->time]),
+            ],
         ];
     }
 }
