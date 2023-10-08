81c15b6ee0512331f47ae84fcc7a6e78664ce494
Joowd <joowdx@gmail.com>
2023-10-08 08:21:46 +0800

    Use laravel process facade

--- app/Services/DownloaderService.php
+++ app/Services/DownloaderService.php
@@ -5,6 +5,8 @@ namespace App\Services;
 use App\Traits\RunsZkScript;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Process;
+use RuntimeException;
 
 class DownloaderService
 {
@@ -14,18 +16,18 @@ class DownloaderService
 
     public function getAttendance(string $from = null, string $to = null, bool $array = true): Collection|array
     {
-        $output = exec($this->command("$from $to"), $response, $code);
+        $process = Process::timeout(120)->run($this->command($from, $to));
 
-        if ($code > 0) {
-            switch (strtolower($output)) {
-                case 'unauthenticated': {
-                    throw new \RuntimeException($this->ping ? 'Invalid password provided.' : 'Cannot reach device or invalid password provided.');
-                }
-                default: throw new \RuntimeException($output ?? '');
+        if ($process->failed()) {
+            switch(trim(strtolower($process->output()))) {
+                case 'unauthenticated':throw new RuntimeException($this->ping ? 'Invalid password provided.' : 'Cannot reach device or invalid password provided.');
+                case 'timed out': throw new RuntimeException('Timed out.' . ($this->ping ? 'Device may be unreachable' : ''));
+                default: throw new RuntimeException($process->output() ?? $process->errorOutput());
             }
         }
 
-        return collect($response)
+        return collect(explode("\n", $process->output()))
+            ->filter()
             ->map(fn ($d) => json_decode($d, true))
             ->when($array, fn ($collection) => $collection->toArray());
     }
