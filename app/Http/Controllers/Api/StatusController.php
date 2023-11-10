d979a3a811671a61237adf006afa6c6b63b45c94
Joowd <joowdx@pm.me>
2023-11-10 10:55:18 +0800

    Add user employee cout

--- app/Http/Controllers/Api/StatusController.php
+++ app/Http/Controllers/Api/StatusController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Enums\UserRole;
 use App\Http\Controllers\Controller;
+use App\Models\Employee;
 use App\Models\Scanner;
 use Illuminate\Http\Request;
 
@@ -21,6 +22,39 @@ class StatusController extends Controller
             UserRole::DEPARTMENT_HEAD => Scanner::with(['lastUpload', 'latestTimelog'])->whereHas('employees', fn ($q) => $q->whereIn('office', $user->offices))->get(),
             default => [],
         };
+
+        $filter = function ($query) use ($request) {
+            if ($request->user()->role === UserRole::DEPARTMENT_HEAD) {
+                $query->whereIn('office', $request->user()->offices);
+            } elseif ($request->all) {
+                if ($request->unenrolled === 'only') {
+                    $query->whereDoesntHave('scanners');
+                } elseif (! $request->unenrolled) {
+                    $query->whereHas('scanners');
+                }
+            } else {
+                $query->whereHas('scanners', function ($query) {
+                    $query->where('enrollments.enabled', true);
+
+                    $query->whereHas('users', function ($query) {
+                        $query->where('user_id', auth()->id());
+                    });
+                });
+            }
+        };
+
+        $employee = fn ($query) => $query
+            ->with('scanners')
+            ->orderBy('name->last')
+            ->orderBy('name->first')
+            ->orderBy('name->middle')
+            ->orderBy('name->extension')
+            ->where($filter)
+            ->whereActive($request->active ?? true)
+            ->when($request->user()->role === UserRole::DEPARTMENT_HEAD, fn ($q) => $q->setEagerLoads([]))
+            ->when($request->filled('office'), fn ($q) => $q->whereOffice(strtolower($request->office)))
+            ->when($request->filled('regular'), fn ($q) => $q->whereRegular($request->regular))
+            ->when($request->filled('group'), fn ($q) => $q->whereJsonContains('groups', strtolower($request->group)));
 
         return [
             'app' => [
@@ -40,6 +74,7 @@ class StatusController extends Controller
                 'scanners_last_update' => $scanners->mapWithKeys(fn ($scanner) => [$scanner->name => $scanner->lastUpload?->time]),
                 'scanners_latest_data' => $scanners->mapWithKeys(fn ($scanner) => [$scanner->name => $scanner->latestTimelog?->time]),
             ],
+            'employees' => Employee::query($employee)->count(),
         ];
     }
 }
