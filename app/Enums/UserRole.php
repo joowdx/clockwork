3ba876f456a494e1b217206d05cdde7bb5c3b574
Joowd <joowdx@pm.me>
2024-01-02 10:11:46 +0800

    Reorder user roles

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -4,25 +4,25 @@ namespace App\Enums;
 
 enum UserRole: int
 {
-    case DEVELOPER = -1;
-    case USER = 0;
-    case ADMINISTRATOR = 1;
     case SYSTEM = 2;
+    case USER = 0;
+    case DEVELOPER = -1;
+    case ADMINISTRATOR = 1;
     case DEPARTMENT_HEAD = 3;
-    case ADMINISTRATIVE_OFFICER = 4;
-    case SECURITY_PERSONNEL = 5;
     case SHIFT_MANAGER = 6;
+    case SECURITY_PERSONNEL = 5;
+    case ADMINISTRATIVE_OFFICER = 4;
 
     public function label(): string
     {
         return match ($this) {
-            self::USER => 'User',
             self::SYSTEM => 'System',
+            self::USER => 'User',
             self::DEVELOPER, self::ADMINISTRATOR => 'Administrator',
             self::DEPARTMENT_HEAD => 'Department Head',
-            self::ADMINISTRATIVE_OFFICER => 'Administrative Officer',
-            self::SECURITY_PERSONNEL => 'Security Personnel',
             self::SHIFT_MANAGER => 'Shift Manager',
+            self::SECURITY_PERSONNEL => 'Security Personnel',
+            self::ADMINISTRATIVE_OFFICER => 'Administrative Officer',
         };
     }
 }
