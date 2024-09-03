a9a26c7ab6f433ea6df6dd6b679306221a503e24
Joowd <joowdx@pm.me>
2024-09-03 09:26:44 +0800

    Add user helper function

--- app/helpers.php
+++ app/helpers.php
@@ -3,16 +3,23 @@
 if (! function_exists('settings')) {
     function settings(?string $key, bool $defaults = true): mixed
     {
-        try {
-            if (is_null($key)) {
-                return null;
-            }
+        if (is_null($key)) {
+            return null;
+        }
 
+        try {
             $value = \App\Models\Setting::get($key);
 
             return $defaults ? ($value ?? \App\Models\Setting::default($key)) : $value;
         } catch (\Illuminate\Database\QueryException) {
             return null;
         }
+    }
+}
+
+if (! function_exists('user')) {
+    function user(): ?\App\Models\User
+    {
+        return \App\Models\User::find(\Illuminate\Support\Facades\Auth::id());
     }
 }
