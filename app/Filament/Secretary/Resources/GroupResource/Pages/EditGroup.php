f99930b98eb85fc9bdf3925a81b53692005861ce
Joowd <joowdx@pm.me>
2024-09-10 10:53:38 +0800

    Add group resource to secretary panel

---
+++ app/Filament/Secretary/Resources/GroupResource/Pages/EditGroup.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Secretary\Resources\GroupResource\Pages;
+
+use App\Filament\Secretary\Resources\GroupResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditGroup extends EditRecord
+{
+    protected static string $resource = GroupResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
---
+++ app/Filament/Secretary/Resources/GroupResource/Pages/ListGroups.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filament\Secretary\Resources\GroupResource\Pages;
+
+use App\Filament\Secretary\Resources\GroupResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+use Illuminate\Contracts\Support\Htmlable;
+
+class ListGroups extends ListRecords
+{
+    protected static string $resource = GroupResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getSubheading(): string|Htmlable|null
+    {
+        $subheading = <<<'HTML'
+            <span class="text-sm text-custom-600 dark:text-custom-400" style="--c-400:var(--warning-400);--c-600:var(--warning-600);">
+                Groups are accessible to all users along with its members.
+            </span>
+        HTML;
+
+        return str($subheading)->toHtmlString();
+    }
+}
