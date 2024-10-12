bb89052b28c63a88b4cd198f180a66b8d176324b
Joowd <joowdx@gmail.com>
2024-10-12 18:52:49 +0800

    Remove user resource  delete bulk action

--- app/Filament/Superuser/Resources/UserResource.php
+++ app/Filament/Superuser/Resources/UserResource.php
@@ -135,11 +135,6 @@ class UserResource extends Resource
             ->actions([
                 Tables\Actions\EditAction::make(),
             ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ])
             ->recordUrl(null);
     }
 
