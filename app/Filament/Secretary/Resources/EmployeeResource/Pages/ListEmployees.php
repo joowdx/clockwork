533c5b177f82d96606f11c5755f23a0c000ad648
Joowd <joowdx@gmail.com>
2024-10-12 17:54:02 +0800

    Remove warning subheading

--- app/Filament/Secretary/Resources/EmployeeResource/Pages/ListEmployees.php
+++ app/Filament/Secretary/Resources/EmployeeResource/Pages/ListEmployees.php
@@ -18,12 +18,14 @@ class ListEmployees extends ListRecords
 
     public function getSubheading(): string|Htmlable|null
     {
-        $subheading = <<<'HTML'
-            <span class="text-sm text-custom-600 dark:text-custom-400" style="--c-400:var(--warning-400);--c-600:var(--warning-600);">
-                This only lists employees who are either deployed or enrolled in your assigned offices or scanners.
-            </span>
-        HTML;
+        // $subheading = <<<'HTML'
+        //     <span class="text-sm text-custom-600 dark:text-custom-400" style="--c-400:var(--warning-400);--c-600:var(--warning-600);">
+        //         This only lists employees who are either deployed or enrolled in your assigned offices or scanners.
+        //     </span>
+        // HTML;
 
-        return str($subheading)->toHtmlString();
+        // return str($subheading)->toHtmlString();
+
+        return null;
     }
 }
--- app/Filament/Secretary/Resources/GroupResource/Pages/ListGroups.php
+++ app/Filament/Secretary/Resources/GroupResource/Pages/ListGroups.php
@@ -20,12 +20,14 @@ class ListGroups extends ListRecords
 
     public function getSubheading(): string|Htmlable|null
     {
-        $subheading = <<<'HTML'
-            <span class="text-sm text-custom-600 dark:text-custom-400" style="--c-400:var(--warning-400);--c-600:var(--warning-600);">
-                Groups are accessible to all users along with its members.
-            </span>
-        HTML;
+        // $subheading = <<<'HTML'
+        //     <span class="text-sm text-custom-600 dark:text-custom-400" style="--c-400:var(--warning-400);--c-600:var(--warning-600);">
+        //         Groups are accessible to all users along with its members.
+        //     </span>
+        // HTML;
 
-        return str($subheading)->toHtmlString();
+        // return str($subheading)->toHtmlString();
+
+        return null;
     }
 }
