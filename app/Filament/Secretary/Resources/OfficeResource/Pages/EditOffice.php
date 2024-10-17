f7d20b404207121799ab5a6c1a92d1cc959461f8
Joowd <joowdx@pm.me>
2024-10-17 09:20:33 +0800

    Fix error when updating department head to not assigned office

--- app/Filament/Secretary/Resources/OfficeResource/Pages/EditOffice.php
+++ app/Filament/Secretary/Resources/OfficeResource/Pages/EditOffice.php
@@ -23,14 +23,20 @@ class EditOffice extends EditRecord
     protected function afterSave(): void
     {
         if (isset($this->record->employee_id)) {
-            Deployment::query()
-                ->where('employee_id', $this->record->employee_id)
-                ->where('office_id', $this->record->id)
-                ->update([
-                    'active' => true,
-                    'current' => true,
-                    'supervisor_id' => null,
-                ]);
+            Deployment::upsert([
+                'employee_id' => $this->record->employee_id,
+                'office_id' => $this->record->id,
+                'active' => true,
+                'current' => true,
+                'supervisor_id' => null,
+            ], [
+                'employee_id',
+                'office_id',
+            ], [
+                'active',
+                'current',
+                'supervisor_id',
+            ]);
 
             Deployment::query()
                 ->where('employee_id', $this->record->employee_id)
