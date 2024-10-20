2ef7ebb66fc989700d94beb005ab702c19b0815d
Joowd <joowdx@gmail.com>
2024-10-20 16:42:34 +0800

    Add employee password reset action

--- app/Filament/Superuser/Resources/EmployeeResource/Pages/EditEmployee.php
+++ app/Filament/Superuser/Resources/EmployeeResource/Pages/EditEmployee.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Superuser\Resources\EmployeeResource\Pages;
 
 use App\Filament\Superuser\Resources\EmployeeResource;
+use App\Models\Employee;
 use Filament\Actions;
+use Filament\Forms\Components\TextInput;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Validation\Rules\Password;
 
 class EditEmployee extends EditRecord
 {
@@ -23,6 +26,34 @@ class EditEmployee extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('reset_password')
+                ->requiresConfirmation()
+                ->modalIcon('heroicon-o-shield-check')
+                ->modalHeading('Password Reset')
+                ->modalDescription('')
+                ->successNotificationTitle('Password reset successful')
+                ->form([
+                    TextInput::make('password')
+                        ->columnSpan(2)
+                        ->password()
+                        ->rule(Password::default())
+                        ->rule('required')
+                        ->markAsRequired()
+                        ->requiredWith('passwordConfirmation')
+                        ->same('passwordConfirmation'),
+                    TextInput::make('passwordConfirmation')
+                        ->columnSpan(2)
+                        ->password()
+                        ->rule('required')
+                        ->markAsRequired()
+                        ->requiredWith('password')
+                        ->dehydrated(false),
+                ])
+                ->action(function (Actions\Action $component, Employee $record, array $data) {
+                    $record->update(['password' => $data['password']]);
+
+                    $component->sendSuccessNotification();
+                }),
             Actions\DeleteAction::make(),
             Actions\ForceDeleteAction::make(),
             Actions\RestoreAction::make(),
