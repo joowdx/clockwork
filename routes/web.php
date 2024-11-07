1e5c6523158d793fde086e0faa078c9281b0efd2
Joowd <joowdx@pm.me>
2024-11-07 13:52:30 +0800

    Remove test function

--- routes/web.php
+++ routes/web.php
@@ -18,24 +18,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('test', fn () => 'Hello World!')->name('test');
 
 Route::get('export/{export}', ExportController::class)->name('export');
-
-Route::get('test', function () {
-    $timesheet = Timesheet::find('01jakwnb0sct0kgq5wfb3w8k17');
-
-    $user = $timesheet->employee;
-
-    $data = [
-        'timesheets' => [$timesheet],
-        'user' => $user,
-        'month' => $timesheet->month,
-        'period' => $timesheet->span,
-        'format' => 'csc',
-        'size' => $data['size'] ?? 'folio',
-        'certify' => 1,
-        'misc' => [
-            'calculate' => true,
-        ],
-    ];
-
-    return view('print.csc', $data);
-});
