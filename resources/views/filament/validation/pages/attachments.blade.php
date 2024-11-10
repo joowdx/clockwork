9c562b4c61d28e509814a4cdeafcc79a1addd7f7
Joowd <joowdx@gmail.com>
2024-11-11 05:22:29 +0800

    Enhance attachment display in validation page with improved layout and PDF viewing options

--- resources/views/filament/validation/pages/attachments.blade.php
+++ resources/views/filament/validation/pages/attachments.blade.php
@@ -2,27 +2,59 @@
 use App\Enums\AttachmentClassification;
 ?>
 
-<section>
+<section class="grid gap-y-2">
     @foreach($attachments as $attachment)
-        <article class="space-y-2">
+        <article class="grid gap-y-2">
             @switch($attachment->classification)
                 @case(AttachmentClassification::ACCOMPLISHMENT)
-                    <h2 class="text-sm font-bold">
-                        {{ $attachment->classification->getLabel() }}
-                    </h2>
-
-                    <x-filament::button
-                        href="{{ route('export', $attachment) }}"
-                        tag="a"
+                    <div
+                        @class(['fi-in-entry-wrp-label inline-flex items-center gap-x-3'])
                     >
-                        Download
-                    </x-filament::button>
+                        <span class="text-sm font-medium leading-6 text-gray-950 dark:text-white">
+                            Accomplishment
+                        </span>
 
-                    <embed
-                        class="w-full rounded-xl h-[36rem]"
-                        src="data:application/pdf;base64,{{base64_encode($attachment->content)}}"
+                        <div class="flex space-x-2">
+                            <x-filament::icon-button
+                                tag="a"
+                                href="{{ route('download.attachment', $attachment) }}"
+                                icon="heroicon-o-arrow-down-tray"
+                                outlined
+                                download
+                            />
+
+                            <x-filament::modal width="7xl">
+                                <x-slot name="trigger">
+                                    <x-filament::icon-button
+                                        icon="heroicon-o-arrow-top-right-on-square"
+                                    />
+                                </x-slot>
+
+                                <iframe
+                                    title="Accomplishment"
+                                    loading="lazy"
+                                    class="w-full rounded-xl h-[calc(100vh-8rem)]"
+                                    src="{{ route('download.attachment', ['attachment' => $attachment, 'inline' => 1]) }}#view=FitH"
+                                    type="application/pdf"
+                                    frameBorder="0"
+                                >
+
+                                </iframe>
+                            </x-filament::modal>
+                        </div>
+                    </div>
+
+                    <iframe
+                        title="Accomplishment"
+                        loading="lazy"
+                        class="w-full rounded-xl"
+                        src="{{ route('download.attachment', ['attachment' => $attachment, 'inline' => 1]) }}#view=FitH&toolbar=0&navpanes=0"
                         type="application/pdf"
+                        height="400"
+                        frameBorder="0"
                     >
+
+                    </iframe>
                 @break
             @endswitch
         </article>
