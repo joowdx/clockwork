af7a441be8788467ff065c3c842da717392a5906
Joowd <joowdx@pm.me>
2024-12-12 10:38:14 +0800

    Add pulse user resolver

--- app/Providers/PulseServiceProvider.php
+++ app/Providers/PulseServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Resolver\PulseUserResolver;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Pulse\Contracts\ResolvesUsers;
 use Laravel\Pulse\PulseServiceProvider as ServiceProvider;
 
 class PulseServiceProvider extends ServiceProvider
@@ -14,6 +17,8 @@ class PulseServiceProvider extends ServiceProvider
     {
         parent::boot();
 
-        Gate::define('viewPulse', fn ($user) => $user->developer && $user->superuser);
+        Gate::define('viewPulse', fn ($user) => true);
+
+        App::singleton(ResolvesUsers::class, PulseUserResolver::class);
     }
 }
