99ca82c514ef5a3f81cca6d920dca65f250123c9
Joowd <joowdx@gmail.com>
2025-01-01 09:30:53 +0800

    Add privacy policy and user agreement links to login page

--- resources/views/filament/auth/login.blade.php
+++ resources/views/filament/auth/login.blade.php
@@ -20,5 +20,28 @@
         />
     </x-filament-panels::form>
 
+    @php($ua = settings('ua'))
+    @php($pp = settings('pp'))
+    @if($ua || $pp)
+        <div class="text-sm text-gray-500">
+            <p class="text-center">
+                By continuing, you agree to our
+                @if($pp)
+                <a href="{{ route('filament.legal.pages.privacy-policy') }}" target="_blank" class="text-blue-500 hover:underline">
+                    Privacy Policy
+                </a>
+                @endif
+                @if ($ua && $pp)
+                    and
+                @endif
+                @if ($ua)
+                    <a href="{{ route('filament.legal.pages.user-agreement') }}" target="_blank" class="text-blue-500 hover:underline">
+                        User Agreement
+                    </a>
+                @endif
+            </p>
+        </div>
+    @endif
+
     {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::AUTH_LOGIN_FORM_AFTER, scopes: $this->getRenderHookScopes()) }}
 @endsection
