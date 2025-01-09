9382dab19b9c5384eede1c2c5cf5122336039a5f
Joowd <joowdx@pm.me>
2025-01-09 09:59:52 +0800

    Refactor mimetype method to improve readability and type hinting for disk storage

--- app/Models/Export.php
+++ app/Models/Export.php
@@ -91,7 +91,10 @@ class Export extends Model
         return Attribute::make(
             function (): ?string {
                 if ($this->disk !== null && in_array($this->disk, ['public', 'local', 'azure'])) {
-                    return Storage::disk($this->disk)->mimetype($this->filename);
+                    /** @var \Illuminate\Filesystem\FilesystemAdapter $disk */
+                    $disk = Storage::disk($this->disk);
+
+                    return $disk->mimetype($this->filename);
                 }
 
                 if ($this->disk === null && file_exists($this->filename)) {
