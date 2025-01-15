65312f0ec8bbebb6063eb2d3655b4b8cd8cdded2
Joowd <joowdx@gmail.com>
2025-01-15 20:52:45 +0800

    Refactor layout for improved responsiveness and consistency

--- resources/views/filament/auth/base.blade.php
+++ resources/views/filament/auth/base.blade.php
@@ -1,9 +1,9 @@
 <div class="min-h-screen flex items-center justify-center relative">
-    <div class="background absolute inset-0 bg-cover bg-center z-0" style="background-image: url('/svg/background.svg');"></div>
+    <div class="bg-cover absolute inset-0 h-full w-full bg-center z-0" style="background-image: url('/svg/background.svg');"></div>
 
     <div class="absolute inset-0 bg-black bg-opacity-30"></div>
 
-    <div class="relative z-10 bg-gray-100 dark:bg-gray-800 rounded-lg overflow-hidden w-full max-w-5xl grid grid-cols-1 md:grid-cols-2">
+    <div class="relative z-10 bg-gray-100 dark:bg-gray-800 lg:rounded-lg overflow-hidden w-full max-w-5xl grid grid-cols-1 md:grid-cols-2">
         <div class="p-8 flex flex-col justify-center">
             <section class="grid auto-cols-fr gap-y-6">
                 <header class="flex flex-col items-center fi-simple-header">
@@ -14,7 +14,7 @@
                     <h1 class="text-3xl font-bold tracking-tight text-center fi-simple-header-heading text-gray-950 dark:text-white">
                         @yield('heading', $this->getHeading())
                     </h1>
-        
+
                     <p class="mt-2 text-sm text-center text-gray-500 fi-simple-header-subheading dark:text-gray-400">
                         @yield('subheading', $this->getSubHeading())
                     </p>
@@ -41,14 +41,3 @@
 
     <x-filament-actions::modals />
 </div>
-
-@push('styles')
-<style>
-    .background {
-        background-size: cover;
-        background-position: center;
-        width: 100%;
-        height: 100%;
-    }
-</style>
-@endpush
