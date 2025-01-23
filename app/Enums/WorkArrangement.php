86b830a4e3c78a56dd51b34f5ac40dde7642682e
Joowd <joowdx@gmail.com>
2025-01-23 19:54:04 +0800

    Remove flexi t ime

--- app/Enums/WorkArrangement.php
+++ app/Enums/WorkArrangement.php
@@ -8,12 +8,12 @@ use Filament\Support\Contracts\HasLabel;
 enum WorkArrangement: string implements HasDescription, HasLabel
 {
     case STANDARD_WORK_HOUR = 'standard-work-hour';
-    case FLEXI_TIME = 'flexi-time';
+    // case FLEXI_TIME = 'flexi-time';
     case WORK_SHIFTING = 'work-shifting';
     // case COMPRESSED_WORK_WEEK = 'compressed-work-week';
     case ROUND_THE_CLOCK = 'round-the-clock';
 
-    // case UNSET = '';
+    case UNSET = '';
 
     public function getLabel(): ?string
     {
@@ -28,7 +28,7 @@ enum WorkArrangement: string implements HasDescription, HasLabel
         return match ($this) {
             self::STANDARD_WORK_HOUR => 'Normal working hours',
             self::WORK_SHIFTING => 'Employees work different shifts without set breaks',
-            self::FLEXI_TIME => 'Custom start and end times within set limits',
+            // self::FLEXI_TIME => 'Custom start and end times within set limits',
             // self::COMPRESSED_WORK_WEEK => 'More hours in fewer days',
             self::ROUND_THE_CLOCK => 'For continuous operation 24h or 48h shifts',
             default => null,
